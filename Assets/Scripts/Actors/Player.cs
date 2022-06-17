1c3f889f3e373516241962fd0e509347bd4a9898
Dmitri Kuznetsov <kna_int@mail.ru>
2022-06-17 22:14:11 +0300

    Added player classes

---
+++ Assets/Scripts/Actors/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Actors
+{
+    public class Player : MonoBehaviour
+    {
+        //Exposed
+
+        [Header("Locator")]
+
+        [Tooltip("Locator")]
+        public PlayerLocator Locator;
+
+        // Status
+
+        [Header("Status")]
+        public PlayerState State;
+    }
+}
